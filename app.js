ce4f87defac186ad8fc3a1d31f229bf6683a8a6f
dvorabu <92503372+dvorabu@users.noreply.github.com>
2022-01-23 15:08:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+const indexRoutes = require('./routes/index');
+const loginRoutes = require('./routes/login');
+const apiRoutes = require('./routes/api');
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(session({
+  secret:"somesecretkey",
+  resave: false, // Force save of session for each request
+  saveUninitialized: false, // Save a session that is new, but has not been modified
+  cookie: {maxAge: 10*60*1000 } // milliseconds!
+}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(loginRoutes);
+app.use('/api', apiRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
